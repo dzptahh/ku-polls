c317796c8b312d8f05f6c1085375c843a7340fdd
dzp-datah <damita.f@ku.th>
2022-09-17 22:46:09 +0700

    add login page and update some errors

--- mysite/urls.py
+++ mysite/urls.py
@@ -15,8 +15,12 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import include, path
+from django.views.generic import RedirectView
 
 urlpatterns = [
+    path('accounts/', include('django.contrib.auth.urls')),
+    path('', RedirectView.as_view(url="/polls/")),
     path('polls/', include('polls.urls')),
     path('admin/', admin.site.urls),
+
 ]
