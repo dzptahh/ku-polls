923cd4c8d1959ba23a2e49971796c83b217c1489
dzp-datah <damita.f@ku.th>
2022-10-18 20:51:59 +0700

    update secret key

--- mysite/settings.py
+++ mysite/settings.py
@@ -14,7 +14,6 @@ from pathlib import Path
 from decouple import config
 import os.path
 
-
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -22,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = config('SECRET_KEY', cast=str)
+SECRET_KEY = config('SECRET_KEY', cast=str, defalut='missing-secret-key')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config('DEBUG', default=True, cast=bool)
